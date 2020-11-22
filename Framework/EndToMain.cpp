60cfce8af8ca09fbfb23c1fa994b4659d79abd40
Kalready <bagzaru@naver.com>
2020-11-23 02:39:52 +0900

    주석 추가

--- Framework/EndToMain.cpp
+++ Framework/EndToMain.cpp
@@ -3,6 +3,8 @@
 #include "StartScene.h"
 #include "InputManager.h"
 
+//화면을 클릭하면 메인 화면으로 이동합니다.
+
 void EndToMain::Update()
 {
 	if (InputManager::GetKeyDown(VK_RETURN))
--- Framework/Enemy.cpp
+++ Framework/Enemy.cpp
@@ -7,16 +7,20 @@
 Enemy::Enemy(const wchar_t* imagePath, float moveSpeed, int hp, Transform* playerTransform)
 	: GameObject(imagePath),hp(hp),moveSpeed(moveSpeed), playerTransform(playerTransform)
 {
+	//충돌을 감지하기 위해 충돌체를 제작합니다.
 	col = new AABBCollider(
 		this,
 		renderer
 	);
+	//충돌체를 현재 Scene에 추가합니다.
 	Scene::PushOnCurrentScene(col);
 
+	//이 오브젝트에 Tag를 등록합니다. Enemy로 등록하였습니다. 이후 충돌처리 때 사용합니다.
 	tag = Tag::Enemy;
 }
 
 Enemy::~Enemy(){
+	//충돌체를 사용할 경우 반드시 초기화가 필요합니다.
 	SAFE_DELETE(col);
 }
 
@@ -33,12 +37,16 @@ void Enemy::Move()
 	transform->position.y += aim.y * moveSpeed * TimeManager::GetDeltaTime();
 }
 
+//OnCollision: 충돌이 감지되었을 때 호출됩니다.
 void Enemy::OnCollision(GameObject* other)
 {
+	//충돌체가 Bullet일 경우에만 체력을 깎습니다.
 	if (other->tag == Tag::Bullet)
 	{
 		--hp;
 	}
+
+	//체력이 0보다 낮을 경우 점수를 더하고 이 객체를 삭제합니다.
 	if (hp <= 0)
 	{
 		Scene::Destroy(this);
