563ce6a39a65be9a807f99be50ad6a7791cb9029
Kalready <bagzaru@naver.com>
2020-12-02 00:14:14 +0900

    주석 추가

--- Framework/Ground.cpp
+++ Framework/Ground.cpp
@@ -24,6 +24,8 @@ bool Ground::CheckStand(AABBCollider* g, float deltaSpeed)
 	float surface = col->boundingBox.leftTop.y * transform->scale.y + transform->position.y;
 	
 	//오브젝트의 발 부분과 땅의 윗 부분 사이의 거리가 gap 이내일 경우, 나의 윗 부분으로 올라왔다 판정합니다.
+	//아래로 내려갈 때, 속도가 너무 빠르면 gap보다 이동 거리가 더 길어 충돌이 무시될 수 있습니다.
+	//이러한 문제를 해결하기 위해 위쪽 방향에 대해서는 속도 값만큼 빼주어 충돌을 확인합니다.
 	//위, 아래만 판정합니다.
 	if (foot - deltaSpeed - surface > -gap && foot - surface < gap)
 	{
